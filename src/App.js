2ab9b028699d81de17b070fe121655631cf94a53
RakeshCharpe <rakeshcharpe99@gmail.com>
2022-03-01 02:39:16 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 
 import './App.css';
+import React,{useState} from 'react'
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
 
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode("dark");
+        
+    } else {
+      setMode('light')
+      
+      }
+  }
  
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About Us" />
+      <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode={ toggleMode}/>
       <div className="container my-3">
 
-      <TextForm heading="Enter the text to analyze below" />
+        <TextForm heading="Enter the text to analyze below" />
+       {/* <About /> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,39 +1,63 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 const TextForm = (props) => {
+  const [text, setText] = useState("Enter the text here");
 
-
-    const [text, setText] = useState("Enter the text here");
-
-    const ChangeHandler = (event) => {
-        setText(event.target.value);
-    }
-    const handleUpClick = () => {
-        setText(text.toUpperCase());
-    }
-    const handleDownClick = () => {
-      setText(text.toLowerCase());
-    };
+  const ChangeHandler = (event) => {
+    setText(event.target.value);
+  };
+  const handleUpClick = () => {
+    setText(text.toUpperCase());
+  };
+  const handleDownClick = () => {
+    setText(text.toLowerCase());
+  };
+  const handleClearClick = () => {
+    setText("");
+  };
+   const handleExtraSpaces = () => {
+     let newText = text.split(/[ ]+/);
+     setText(newText.join(" "));
+   };
   return (
-    <div>
-      <h2>{props.heading}</h2>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          id="myBox"
-          rows="8"
-          onChange={ChangeHandler}
-        ></textarea>
+    <>
+      <div className="container">
+        <h2>{props.heading}</h2>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            id="myBox"
+            rows="8"
+            onChange={ChangeHandler}
+          ></textarea>
+        </div>
+        <button className="btn btn-info " onClick={handleUpClick}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-info mx-3" onClick={handleDownClick}>
+          Convert to LowerCase
+        </button>
+        <button className="btn btn-info mx-3" onClick={handleClearClick}>
+          Clear Text
+        </button>
+        <button className="btn btn-info mx-3" onClick={handleExtraSpaces}>
+          Remove Extra Spaces
+        </button>
       </div>
-      <button className="btn btn-primary " onClick={handleUpClick}>
-        Convert to UpperCase
-      </button>
-      <button className="btn btn-primary " onClick={handleDownClick}>
-        Convert to LowerCase
-      </button>
-    </div>
-  );
-}
+      <hr />
+      <div className="container my-3">
+        <h1>Your text Summary</h1>
 
-export default TextForm
+        <p>
+          Total {text.split(" ").length} words and {text.length} Characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes to read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+};
+
+export default TextForm;
